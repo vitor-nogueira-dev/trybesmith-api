ef6acc0e8ecff34a7369226c5f546cd827ae99cc
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-26 09:58:25 -0300

    refactor: authMiddleware
    logical extraction token validation for another user Is Logged

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,41 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 
-import jwtUtil from 'src/utils/jwt.util';
+import CustomError from '../utils/custom.error';
 import UserModel from '../database/models/user.model';
 
-class CustomError extends Error {
-  statusCode: number;
-
-  constructor(statusCode: number, message: string) {
-    super(message);
-    this.statusCode = statusCode;
-  }
-}
-
-function tokenEmptyField(token: string): void {
-  if (!token) {
-    throw new CustomError(401, 'Token not found');
-  }
-}
-
-async function userIsLogged(token: string) {
-  const decoded = jwtUtil.verify(token);
-  const user = await UserModel.findOne({ where: { username: decoded.username } });
-  if (!user) {
-    throw new CustomError(401, 'Invalid token');
-  }
-}
-
-function validUserIdBody(userId: number) {
+async function validUserIdBody(userId: number): Promise<void> {
   if (!userId) {
     throw new CustomError(400, '"userId" is required');
   }
+
   if (typeof userId !== 'number') {
-    throw new CustomError(400, '"userId" must be a number');
+    throw new CustomError(422, '"userId" must be a number');
   }
-  const userExists = UserModel.findByPk(userId);
-  if (!userExists) {
-    throw new CustomError(404, 'User does not exist');
+
+  const user = await UserModel.findByPk(userId);
+  if (!user) {
+    throw new CustomError(404, '"userId" not found');
   }
 }
 
@@ -44,22 +23,17 @@ function validProductIds(productIds: number[]) {
     throw new CustomError(400, '"productIds" is required');
   }
   if (!Array.isArray(productIds)) {
-    throw new CustomError(400, '"productIds" must be an array');
+    throw new CustomError(422, '"productIds" must be an array');
   }
   if (productIds.length === 0) {
-    throw new CustomError(400, '"productIds" must include only numbers');
+    throw new CustomError(422, '"productIds" must include only numbers');
   }
 }
 
-function validInsertOrders(req: Request, res: Response, next: NextFunction) {
+async function validInsertOrders(req: Request, res: Response, next: NextFunction) {
   try {
     const { userId, productIds } = req.body;
-    const { authorization } = req.headers;
-    if (authorization !== undefined) {
-      tokenEmptyField(authorization);
-      userIsLogged(authorization);
-    }
-    validUserIdBody(userId);
+    await validUserIdBody(userId);
     validProductIds(productIds);
     return next();
   } catch (error) {
