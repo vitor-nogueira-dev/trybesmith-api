b74e2fe697d4d9448fb3655e9d7cf918bd3fb0a8
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-26 10:05:05 -0300

    feat: router post orders
    creating a route to insert orders into the database

--- src/routers/orders.routes.ts
+++ src/routers/orders.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
+import userisLoggedMiddleware from '../middlewares/userisLogged.middleware';
 import ordersController from '../controllers/orders.controller';
+import authMiddleware from '../middlewares/auth.middleware';
 
 const ordersRouter = Router() as Router;
 
 ordersRouter.get('/', ordersController.getOrders);
+ordersRouter.post(
+  '/',
+  userisLoggedMiddleware.userIsLogged,
+  authMiddleware.validInsertOrders,
+  ordersController.createOrder,
+);
 
 export default ordersRouter;
